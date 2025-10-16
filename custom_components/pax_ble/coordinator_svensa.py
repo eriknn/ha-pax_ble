9153b217635b6a80e1290f84e476a4702626ae79
Yuliyan Tsvetkov <yuliyan.tsvetkov@gmail.com>
2025-10-16 21:26:58 +0300

    Fixing multiple stability issues

--- custom_components/pax_ble/coordinator_svensa.py
+++ custom_components/pax_ble/coordinator_svensa.py
@@ -23,62 +23,64 @@ class SvensaCoordinator(BaseCoordinator):
         _LOGGER.debug("Initializing Svensa!")
         self._fan = Svensa(hass, mac, pin)
 
+        # Set up disconnect callback
+        self._fan.set_disconnect_callback(self._on_device_disconnect)
+
     async def read_sensordata(self, disconnect=False) -> bool:
         _LOGGER.debug("Reading sensor data")
         try:
             # Make sure we are connected
             if not await self._safe_connect():
-                raise Exception("Not connected!")
-        except Exception as e:
-            _LOGGER.warning("Error when fetching config data: %s", str(e))
-            return False
+                _LOGGER.warning("Cannot read sensor data: not connected to %s", self.devicename)
+                return False
 
-        FanState = await self._fan.getState()  # Sensors
-        BoostMode = await self._fan.getBoostMode()  # Sensors?
-        Pause = await self._fan.getPause()
+            FanState = await self._fan.getState()  # Sensors
+            BoostMode = await self._fan.getBoostMode()  # Sensors?
+            Pause = await self._fan.getPause()
 
-        if FanState is None:
-            _LOGGER.debug("Could not read data")
-            return False
-        else:
-            self._state["humidity"] = FanState.Humidity
-            self._state["airquality"] = FanState.AirQuality
-            self._state["temperature"] = FanState.Temp
-            self._state["light"] = FanState.Light
-            self._state["rpm"] = FanState.RPM
-            if FanState.RPM > 400:
-                self._state["flow"] = int(FanState.RPM * 0.05076 - 14)
+            if FanState is None:
+                _LOGGER.debug("Could not read data")
+                return False
             else:
-                self._state["flow"] = 0
-            self._state["state"] = FanState.Mode
+                self._state["humidity"] = FanState.Humidity
+                self._state["airquality"] = FanState.AirQuality
+                self._state["temperature"] = FanState.Temp
+                self._state["light"] = FanState.Light
+                self._state["rpm"] = FanState.RPM
+                if FanState.RPM > 400:
+                    self._state["flow"] = int(FanState.RPM * 0.05076 - 14)
+                else:
+                    self._state["flow"] = 0
+                self._state["state"] = FanState.Mode
 
-            self._state["boostmode"] = BoostMode.OnOff
-            self._state["boostmodespeedread"] = BoostMode.Speed
-            self._state["boostmodesecread"] = BoostMode.Seconds
+                self._state["boostmode"] = BoostMode.OnOff
+                self._state["boostmodespeedread"] = BoostMode.Speed
+                self._state["boostmodesecread"] = BoostMode.Seconds
 
-            self._state["pause"] = Pause.PauseActive
-            self._state["pauseminread"] = Pause.PauseMinutes
-            if not Pause.PauseActive:
-                self._state["pausemin"] = Pause.PauseMinutes
+                self._state["pause"] = Pause.PauseActive
+                self._state["pauseminread"] = Pause.PauseMinutes
+                if not Pause.PauseActive:
+                    self._state["pausemin"] = Pause.PauseMinutes
 
-        if disconnect:
-            await self._fan.disconnect()
-        return True
+            if disconnect:
+                await self._fan.disconnect()
+            return True
+
+        except Exception as e:
+            _LOGGER.warning("Error reading sensor data from %s: %s", self.devicename, str(e))
+            return False
 
     async def write_data(self, key) -> bool:
         _LOGGER.debug("Write_Data: %s", key)
         try:
             # Make sure we are connected
             if not await self._safe_connect():
-                raise Exception("Not connected!")
-        except Exception as e:
-            _LOGGER.warning("Error when writing data: %s", str(e))
-            return False
+                _LOGGER.warning("Cannot write data: not connected to %s", self.devicename)
+                return False
 
-        # Authorize
-        await self._fan.authorize()
+            # Authorize
+            await self._fan.authorize()
 
-        try:
             # Write data
             match key:
                 case "airing" | "fanspeed_airing":
@@ -135,21 +137,19 @@ class SvensaCoordinator(BaseCoordinator):
                 case _:
                     return False
 
-        except Exception as e:
-            _LOGGER.debug("Not able to write command: %s", str(e))
-            return False
+            self.setFastPollMode()
+            return True
 
-        self.setFastPollMode()
-        return True
+        except Exception as e:
+            _LOGGER.warning("Error writing data to %s: %s", self.devicename, str(e))
+            return False
 
     async def read_configdata(self, disconnect=False) -> bool:
         try:
             # Make sure we are connected
             if not await self._safe_connect():
-                raise Exception("Not connected!")
-        except Exception as e:
-            _LOGGER.warning("Error when fetching config data: %s", str(e))
-            return False
+                _LOGGER.warning("Cannot read config data: not connected to %s", self.devicename)
+                return False
 
         AutomaticCycles = await self._fan.getAutomaticCycles()  # Configuration
         self._state["airing"] = AutomaticCycles.TimeMin
@@ -184,12 +184,16 @@ class SvensaCoordinator(BaseCoordinator):
             # Only useful if we start the integration while the fan is paused. Will be re-read when pause ends.
             self._state["pausemin"] = 60
 
-        TimeFunctions = await self._fan.getTimerFunctions()  # Configuration
-        self._state["timer_runtime"] = TimeFunctions.PresenceTime
-        self._state["timer_delay"] = TimeFunctions.TimeMin
-        self._state["fanspeed_sensor"] = TimeFunctions.Speed
-        _LOGGER.debug(f"Time Functions: {TimeFunctions}")
+            TimeFunctions = await self._fan.getTimerFunctions()  # Configuration
+            self._state["timer_runtime"] = TimeFunctions.PresenceTime
+            self._state["timer_delay"] = TimeFunctions.TimeMin
+            self._state["fanspeed_sensor"] = TimeFunctions.Speed
+            _LOGGER.debug(f"Time Functions: {TimeFunctions}")
 
-        if disconnect:
-            await self._fan.disconnect()
-        return True
+            if disconnect:
+                await self._fan.disconnect()
+            return True
+
+        except Exception as e:
+            _LOGGER.warning("Error reading config data from %s: %s", self.devicename, str(e))
+            return False
